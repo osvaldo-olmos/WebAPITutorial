7580d2bfecefc6eb8d84ff8a1baca83a67bedd28
osvaldo-olmos <osvaldo.olmos@gmail.com>
2018-11-25 13:42:15 -0300

    Adding custom validation

--- DTOs/CreateTaskDTO.cs
+++ DTOs/CreateTaskDTO.cs
@@ -1,7 +1,21 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace TodoApi.DTOs
 {
-    public class CreateTaskDTO
+    public class CreateTaskDTO : IValidatableObject
     {
         public string Name {get; set;}
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+        List<ValidationResult> results=new List<ValidationResult>();
+            if (Name.Equals("sarasa"))
+            {
+                results.Add(new ValidationResult("Sea serio, y pongale un nombre como la gente a la tarea.",
+                                        new[] { "Name" }));
+            }
+            return results;
+        }
     }
 }
