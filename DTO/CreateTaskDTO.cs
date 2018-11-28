cdd667564c26b3f2f2b2d349a32a96f1157db5ab
oolmos <oolmos@xsidesolutions.com>
2018-11-27 21:34:59 -0300

    Underposting banned with required and nullable. Custom validation on POST for name added

--- DTO/CreateTaskDTO.cs
+++ DTO/CreateTaskDTO.cs
@@ -1,9 +1,23 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace TaskApi.DTO
 {
-    public class CreateTaskDTO
+    public class CreateTaskDTO : IValidatableObject
     {
+        [Required]
         public string Name { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> results = new List<ValidationResult>();
+            if (Name.Equals("sarasa"))
+            {
+                results.Add(new ValidationResult("Sea serio, y pongale un nombre como la gente a la tarea.",
+                                     new[] { "Name" }));
+            }
+            return results;
+        }
+
     }
 }
--- DTO/UpdateTaskDTO.cs
+++ DTO/UpdateTaskDTO.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TodoApi.DTO
 {
     public class UpdateTaskDTO
     {
+        [Required]
         public string Name { get; set; }
-        public int Status { get; set; }
+        
+        [Required]
+        [Range(0,4)]
+        public int? Status { get; set; }
     }
 }
